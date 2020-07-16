368975437c88c15b16d09bcf310ec521d7c4b160
Veeresh <veeresh9211@gmail.com>
2020-07-16 15:08:13 +0530

    Updates

---
+++ precache-manifest.c7abd92b2d29895281eeb1a9c121078b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "902b33bb4cf16b110d4affddf5ad5f42",
+    "url": "/reactReduxFireBaseCRUD/index.html"
+  },
+  {
+    "revision": "998f0acd451766ca6937",
+    "url": "/reactReduxFireBaseCRUD/static/css/2.591d4c47.chunk.css"
+  },
+  {
+    "revision": "8fde7a419bcee8913300",
+    "url": "/reactReduxFireBaseCRUD/static/css/main.c7571e02.chunk.css"
+  },
+  {
+    "revision": "998f0acd451766ca6937",
+    "url": "/reactReduxFireBaseCRUD/static/js/2.63346eea.chunk.js"
+  },
+  {
+    "revision": "c2abccf36c6f335230488ae126a9af91",
+    "url": "/reactReduxFireBaseCRUD/static/js/2.63346eea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fde7a419bcee8913300",
+    "url": "/reactReduxFireBaseCRUD/static/js/main.58e129ac.chunk.js"
+  },
+  {
+    "revision": "7a31e084fe1dbd70802d",
+    "url": "/reactReduxFireBaseCRUD/static/js/runtime-main.c6b1f75b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactReduxFireBaseCRUD/static/media/logo.5d5d9eef.svg"
+  }
+]);
