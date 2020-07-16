a289548af64dd26dcedf912bf97e79cf885ea3da
Veeresh <veeresh9211@gmail.com>
2020-07-16 15:03:11 +0530

    build changes

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -3,6 +3,9 @@ import {Link} from 'react-router-dom';
 const navBar=()=>{
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
           <h3 className="nav-item active">
             <Link to="/" className="nav-link">Redux TODO</Link>
           </h3>           
